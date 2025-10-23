95b3b82ca7baccfb75ae07faf179ab637435410e
mr-leo44 <lionnelkaniki44@gmail.com>
2025-10-23 12:50:56 +0100

    feat(docs): add dedoc/scramble
    * add to vendor
    * convert config
    * add application on provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,15 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use App\Repositories\UserRepositoryInterface;
-use App\Repositories\EloquentUserRepository;
-use App\Services\AuthServiceInterface;
+use Dedoc\Scramble\Scramble;
 use App\Services\AuthService;
+use App\Services\AuthServiceInterface;
+use Illuminate\Support\ServiceProvider;
+use App\Repositories\EloquentUserRepository;
+use App\Repositories\UserRepositoryInterface;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Dedoc\Scramble\Support\Generator\SecurityRequirement;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +28,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Scramble::configure()
+        ->withDocumentTransformers(function (OpenApi $openApi) {
+            $openApi->components->securitySchemes['bearer'] = SecurityScheme::http('bearer');
+
+            $openApi->security[] = new SecurityRequirement([
+                'bearer' => [],
+            ]);
+        });
     }
 }

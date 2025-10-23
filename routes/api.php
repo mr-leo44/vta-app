bc0bdcfbedddda4ce12c4c4d839371c2522f8dda
mr-leo44 <lionnelkaniki44@gmail.com>
2025-10-23 08:19:46 +0100

    feat(route): add forgotten logout

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');

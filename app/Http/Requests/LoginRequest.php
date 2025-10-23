089f625e499790e5d9d34c6c8ff8379de7202ea0
mr-leo44 <lionnelkaniki44@gmail.com>
2025-10-23 15:24:49 +0100

    feat(request) : update request files
    *  drop MustVerifyEmail interface
    *  add rate limiter on request
    * add middleware to api route

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Support\Str;
+use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
@@ -21,5 +25,52 @@ class LoginRequest extends FormRequest
             'username' => ['required', 'string'],
             'password' => ['required', 'string'],
         ];
+    }
+
+    /**
+     * Vérifie si l'utilisateur est bloqué avant même la validation.
+     */
+    protected function prepareForValidation(): void
+    {
+        $this->ensureIsNotRateLimited();
+    }
+
+    /**
+     * Incrémente le compteur en cas d'échec.
+     */
+    public function failedLogin(): void
+    {
+        RateLimiter::hit($this->throttleKey());
+    }
+
+    /**
+     * Réinitialise le compteur en cas de succès.
+     */
+    public function successfulLogin(): void
+    {
+        RateLimiter::clear($this->throttleKey());
+    }
+
+    protected function ensureIsNotRateLimited(): void
+    {
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            return;
+        }
+
+        event(new Lockout($this));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw ValidationException::withMessages([
+            'username' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
+            ]),
+        ]);
+        }
+
+    public function throttleKey(): string
+    {
+        return Str::lower($this->input('username')).'|'.$this->ip();
     }
 }

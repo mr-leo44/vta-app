11ddc2b5caa1b8e2fe202de11f611e943721b6b7
mr-leo44 <lionnelkaniki44@gmail.com>
2025-10-27 08:01:24 +0100

    feat: define aircrafts routes including find and by name or sigle

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\AircraftController;
 use App\Http\Controllers\Api\OperatorController;
+use App\Http\Controllers\Api\AircraftTypeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -18,7 +19,11 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 Route::get('operators/search', [OperatorController::class, 'search'])->name('operators.search');
 Route::apiResource('operators', OperatorController::class);
 
-Route::get('aircrafts/search', [AircraftController::class, 'search'])->name('aircrafts.search');
+Route::get('aircraft-types/find/{query}', [AircraftTypeController::class, 'find'])->name('aircraft-types.find');
+Route::apiResource('aircraft-types', AircraftTypeController::class);
+
+
 Route::get('operators/{operator}/aircrafts', [AircraftController::class, 'byOperator'])->name('aircrafts.byOperator');
+Route::get('aircrafts/search', [AircraftController::class, 'search'])->name('aircrafts.search');
 Route::apiResource('aircrafts', AircraftController::class);
 

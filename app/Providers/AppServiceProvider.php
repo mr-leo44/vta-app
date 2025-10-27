1974ec8cb31aa306fc3509bcf4e6ee51f3a0ef95
mr-leo44 <lionnelkaniki44@gmail.com>
2025-10-27 08:03:14 +0100

    feat: bind aircraft Type Service and repo interface to his  impl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,16 +4,24 @@ namespace App\Providers;
 
 use Dedoc\Scramble\Scramble;
 use App\Services\AuthService;
+use App\Services\AircraftService;
 use App\Services\OperatorService;
+use App\Services\AircraftTypeService;
 use App\Services\AuthServiceInterface;
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\AircraftRepository;
 use App\Repositories\OperatorRepository;
+use App\Services\AircraftServiceInterface;
 use App\Services\OperatorServiceInterface;
+use App\Repositories\AircraftTypeRepository;
 use App\Repositories\EloquentUserRepository;
 use App\Repositories\UserRepositoryInterface;
 use Dedoc\Scramble\Support\Generator\OpenApi;
+use App\Services\AircraftTypeServiceInterface;
+use App\Repositories\AircraftRepositoryInterface;
 use App\Repositories\OperatorRepositoryInterface;
 use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use App\Repositories\AircraftTypeRepositoryInterface;
 use Dedoc\Scramble\Support\Generator\SecurityRequirement;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,10 +33,12 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(UserRepositoryInterface::class, EloquentUserRepository::class);
         $this->app->bind(AuthServiceInterface::class, AuthService::class);
-        $this->app->bind(\App\Repositories\OperatorRepositoryInterface::class, \App\Repositories\OperatorRepository::class);
-        $this->app->bind(\App\Services\OperatorServiceInterface::class, \App\Services\OperatorService::class);
-        $this->app->bind(\App\Repositories\AircraftRepositoryInterface::class, \App\Repositories\AircraftRepository::class);
-        $this->app->bind(\App\Services\AircraftServiceInterface::class, \App\Services\AircraftService::class);
+        $this->app->bind(OperatorRepositoryInterface::class, OperatorRepository::class);
+        $this->app->bind(OperatorServiceInterface::class, OperatorService::class);
+        $this->app->bind(AircraftRepositoryInterface::class, AircraftRepository::class);
+        $this->app->bind(AircraftServiceInterface::class, AircraftService::class);
+        $this->app->bind(AircraftTypeRepositoryInterface::class, AircraftTypeRepository::class);
+        $this->app->bind(AircraftTypeServiceInterface::class, AircraftTypeService::class);
     }
 
     /**

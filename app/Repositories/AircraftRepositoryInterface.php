521dc76ed61b41d4ec67e0cac210df82ede018da
mr-leo44 <lionnelkaniki44@gmail.com>
2025-11-14 19:17:46 +0100

    feat: update all aircraft module process
    * Update repositories by adding one methode of retrieving all aircrafts
    * Update Services by adding the all method
    * update controller by ading the one method
    * update api route by adding the all method

--- app/Repositories/AircraftRepositoryInterface.php
+++ app/Repositories/AircraftRepositoryInterface.php
@@ -3,12 +3,13 @@
 namespace App\Repositories;
 
 use App\Models\Aircraft;
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 interface AircraftRepositoryInterface
 {
-    public function all(): LengthAwarePaginator;
+    public function all(): Collection;
+    public function allPaginated(): LengthAwarePaginator;
     public function search(string $immatriculation): ?LengthAwarePaginator;
     public function create(array $data): Aircraft;
     public function update(Aircraft $aircraft, array $data): Aircraft;

3476077c49b7665939b4abfd6ef5db00a262574b
mr-leo44 <lionnelkaniki44@gmail.com>
2025-11-23 01:32:32 +0100

    update flight justification eloquent retrieve

--- app/Repositories/FlightJustificationRepository.php
+++ app/Repositories/FlightJustificationRepository.php
@@ -5,28 +5,90 @@ namespace App\Repositories;
 use App\Models\FlightJustification;
 use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * Interface for repositories that handle FlightJustification models.
+ */
+interface FlightJustificationRepositoryInterface
+{
+    /**
+     * Retrieve all FlightJustification models.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function all(): Collection;
+
+    /**
+     * Create a new FlightJustification model.
+     *
+     * @param array $data
+     * @return \App\Models\FlightJustification
+     */
+    public function create(array $data): FlightJustification;
+
+    /**
+     * Update a FlightJustification model.
+     *
+     * @param \App\Models\FlightJustification $justification
+     * @param array $data
+     * @return \App\Models\FlightJustification
+     */
+    public function update(FlightJustification $justification, array $data): FlightJustification;
+
+    /**
+     * Delete a FlightJustification model.
+     *
+     * @param \App\Models\FlightJustification $justification
+     */
+    public function delete(FlightJustification $justification): void;
+}
+
+
+/**
+ * Repository for FlightJustification models.
+ */
 class FlightJustificationRepository implements FlightJustificationRepositoryInterface
 {
+    /**
+     * Retrieve all FlightJustification models.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public function all(): Collection
     {
-        return FlightJustification::all();
+        return FlightJustification::latest()->get();
     }
 
+    /**
+     * Create a new FlightJustification model.
+     *
+     * @param array $data
+     * @return \App\Models\FlightJustification
+     */
     public function create(array $data): FlightJustification
     {
         return FlightJustification::create($data);
     }
 
+    /**
+     * Update a FlightJustification model.
+     *
+     * @param \App\Models\FlightJustification $justification
+     * @param array $data
+     * @return \App\Models\FlightJustification
+     */
     public function update(FlightJustification $justification, array $data): FlightJustification
     {
         $justification->update($data);
         return $justification;
     }
 
+    /**
+     * Delete a FlightJustification model.
+     *
+     * @param \App\Models\FlightJustification $justification
+     */
     public function delete(FlightJustification $justification): void
     {
         $justification->delete();
     }
 }
-
-

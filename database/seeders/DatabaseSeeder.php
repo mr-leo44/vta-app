0a60a555e188895fe22be315d0339c3dafad6e28
mr-leo44 <lionnelkaniki44@gmail.com>
2025-12-08 20:35:34 +0100

    feat: update seeder for first user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,5 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([PermissionRoleSeeder::class]);
+        if (User::count() === 0) {
+            User::factory()->create([
+                'name' => 'Administrateur',
+                'username' => 'admin',
+                'password' => Hash::make('admin@1234!'),
+            ])->assignRole('Admin');
+        }
     }
 }

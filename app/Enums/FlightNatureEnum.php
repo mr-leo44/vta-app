8f7eab04de8729f077aba1bf18c2171a9a0b00a8
mr-leo44 <lionnelkaniki44@gmail.com>
2025-12-16 14:30:29 +0100

    update flightNatureEnum to adapt commercial and non commercial flghts

--- app/Enums/FlightNatureEnum.php
+++ app/Enums/FlightNatureEnum.php
@@ -5,14 +5,51 @@ namespace App\Enums;
 enum FlightNatureEnum: string
 {
     case COMMERCIAL = 'commercial';
-    case NON_COMMERCIAL = 'non_commercial';
+    case STATE = 'state';
+    case REQUISITION = 'requisition';
+    case TEST = 'test';
+    case AFREIGHTMENT = 'afreightment';
+    case HUMANITARE = 'humanitare';
 
     public function label(): string
     {
         return match($this) {
-            self::COMMERCIAL => 'Vol commercial',
-            self::NON_COMMERCIAL => 'Vol non commercial',
+            self::COMMERCIAL => 'Vol Commercial',
+            self::STATE => 'Vol d\'État',
+            self::REQUISITION => 'Vol de Réquisition',
+            self::AFREIGHTMENT => 'Vol d\'Affrètement',
+            self::TEST => 'Vol de Test',
+            self::HUMANITARE => 'Vol Humanitaire',
         };
+    }
+
+    public function isCommercial(): bool
+    {
+        return $this === self::COMMERCIAL;
+    }
+
+    public function isNonCommercial(): bool
+    {
+        return !$this->isCommercial();
+    }
+
+    /**
+     * Get all commercial cases
+     */
+    public static function commercial(): array
+    {
+        return [self::COMMERCIAL];
+    }
+
+    /**
+     * Get all non-commercial cases
+     */
+    public static function nonCommercial(): array
+    {
+        return array_filter(
+            self::cases(),
+            fn($case) => $case->isNonCommercial()
+        );
     }
 
     public static function labels(): array

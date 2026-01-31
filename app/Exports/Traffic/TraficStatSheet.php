b747ba59823ded4f0e31053dd5afb64829f52c93
mr-leo44 <lionnelkaniki44@gmail.com>
2026-01-31 11:31:41 +0100

    feat: update date print

--- app/Exports/Traffic/TraficStatSheet.php
+++ app/Exports/Traffic/TraficStatSheet.php
@@ -77,7 +77,7 @@ class TraficStatSheet implements FromArray, ShouldAutoSize, WithTitle, WithEvent
 
         // ✅ DONNÉES : On ne met QUE les dates, le reste sera écrit dans AfterSheet
         foreach ($this->rows as $row) {
-            $dataRow = [$row['date'] ?? ''];
+            $dataRow = [$row['DATE'] ?? ''];
             // Remplir le reste avec des chaînes vides (temporaire)
             for ($i = 1; $i < $cols; $i++) {
                 $dataRow[] = '';

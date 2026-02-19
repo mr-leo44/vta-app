adea2018090d2f4ded6b6f0256e2104dcfc99e90
mr-leo44 <lionnelkaniki44@gmail.com>
2026-02-19 21:40:20 +0100

    feat(idef-report): add pax and ecart sheets for domestic and international regime

--- app/Exports/Idef/IdefReportExport.php
+++ app/Exports/Idef/IdefReportExport.php
@@ -2,6 +2,8 @@
 
 namespace App\Exports\Idef;
 
+use App\Exports\Idef\EcartStatSheet;
+use App\Exports\Idef\PAXStatSheet;
 use App\Exports\Idef\SynthStatSheet;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
@@ -28,6 +30,64 @@ class IdefReportExport implements WithMultipleSheets
         $selectedMonth = $this->getMonth($this->month);
         $year = $this->year;
         return [
+            new PAXStatSheet(
+                'PAX NAT',
+                "STATISTIQUE GO PASS RAMASSES NATIONAL $selectedMonth $year",
+                $this->domesticData['pax'],
+                $this->domesticData['operators']['pax']
+            ),
+            new PAXStatSheet(
+                'PAX INTER',
+                "STATISTIQUE GO PASS RAMASSES INTERNATIONAL $selectedMonth $year",
+                $this->internationaldata['pax'],
+                $this->internationaldata['operators']['pax']
+            ),
+            new EcartStatSheet(
+                'ECART NAT',
+                "SITUATION  PASSAGERS EMBARQUES ET  GO-PASS  RAMASSES NATIONAL $selectedMonth $year",
+                $this->domesticData['pax']
+            ),
+            new EcartStatSheet(
+                'ECART INT',
+                "SITUATION  PASSAGERS EMBARQUES ET  GO-PASS  RAMASSES INTERNATIONAL $selectedMonth $year",
+                $this->internationaldata['pax']
+            ),
+            // new TraficStatSheet(
+            //     'FRET NAT',
+            //     "EXCEDENT FRET INTERNATIONAL DEPART $selectedMonth $year",
+            //     $this->internationaldata['exced_depart'],
+            //     $this->internationaldata['operators']['fret']
+            // ),
+            // new TraficStatSheet(
+            //     'EXCED FRET NAT',
+            //     "FRET INTERNATIONAL ARRIVEE $selectedMonth $year",
+            //     $this->internationaldata['fret_arrivee'],
+            //     $this->internationaldata['operators']['fret']
+            // ),
+            // new TraficStatSheet(
+            //     'FRET EXON NAT',
+            //     "EXCEDENT FRET INTERNATIONAL ARRIVEE $selectedMonth $year",
+            //     $this->internationaldata['exced_arrivee'],
+            //     $this->internationaldata['operators']['fret']
+            // ),
+            // new TraficStatSheet(
+            //     'FRET INT',
+            //     "EXCEDENT FRET INTERNATIONAL DEPART $selectedMonth $year",
+            //     $this->internationaldata['exced_depart'],
+            //     $this->internationaldata['operators']['fret']
+            // ),
+            // new TraficStatSheet(
+            //     'EXCED FRET INT',
+            //     "FRET INTERNATIONAL ARRIVEE $selectedMonth $year",
+            //     $this->internationaldata['fret_arrivee'],
+            //     $this->internationaldata['operators']['fret']
+            // ),
+            // new TraficStatSheet(
+            //     'FRET EXON INT',
+            //     "EXCEDENT FRET INTERNATIONAL ARRIVEE $selectedMonth $year",
+            //     $this->internationaldata['exced_arrivee'],
+            //     $this->internationaldata['operators']['fret']
+            // ),
             new SynthStatSheet(
                 'SYNTH',
                 "SYNTHESE STATISTIQUES MENSUELLES IDEF $selectedMonth $year",

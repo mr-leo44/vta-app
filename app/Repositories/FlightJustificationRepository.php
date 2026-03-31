60d22ccddbacb2cb2c18dabd54ec82621dd4723c
mr-leo44 <lionnelkaniki44@gmail.com>
2026-03-31 13:15:37 +0100

    fix: remove double implement of flightJustificationInterface

--- app/Repositories/FlightJustificationRepository.php
+++ app/Repositories/FlightJustificationRepository.php
@@ -6,44 +6,6 @@ use App\Models\FlightJustification;
 use Illuminate\Database\Eloquent\Collection;
 
 /**
- * Interface for repositories that handle FlightJustification models.
- */
-interface FlightJustificationRepositoryInterface
-{
-    /**
-     * Retrieve all FlightJustification models.
-     *
-     * @return \Illuminate\Database\Eloquent\Collection
-     */
-    public function all(): Collection;
-
-    /**
-     * Create a new FlightJustification model.
-     *
-     * @param array $data
-     * @return \App\Models\FlightJustification
-     */
-    public function create(array $data): FlightJustification;
-
-    /**
-     * Update a FlightJustification model.
-     *
-     * @param \App\Models\FlightJustification $justification
-     * @param array $data
-     * @return \App\Models\FlightJustification
-     */
-    public function update(FlightJustification $justification, array $data): FlightJustification;
-
-    /**
-     * Delete a FlightJustification model.
-     *
-     * @param \App\Models\FlightJustification $justification
-     */
-    public function delete(FlightJustification $justification): void;
-}
-
-
-/**
  * Repository for FlightJustification models.
  */
 class FlightJustificationRepository implements FlightJustificationRepositoryInterface
